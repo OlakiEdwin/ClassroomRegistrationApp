2ef2efb7585f4b2f8cbab62bef2137a588387990
Olaki Edwin <olakiedwin@gmail.com>
2024-02-28 21:13:05 +0300

    Added Student setup details to main code

--- test.py
+++ test.py
@@ -12,6 +12,8 @@ ScreenManager:
     LoginScreen:
     ResetScreen:
     SignupScreen:
+    StudentSetupScreen:
+    
 
 <MainScreen>
     name: "main"
@@ -446,6 +448,9 @@ ScreenManager:
             height: "40dp"
             size_hint: .66, .065
             pos_hint: {"center_x": .5, "center_y": .3} 
+            on_release:
+                root.manager.transition.direction = "left"
+                root.manager.current = "studentSetup"
             
             MDButtonText:
                 id: text
@@ -488,6 +493,113 @@ ScreenManager:
                 id: text
                 text: "SIGN IN"
                 pos_hint: {"center_x": .5, "center_y": .5}
+                
+<StudentSetupScreen>:
+    name: "studentSetup"
+    
+    MDScreen:
+        md_bg_color: 1, 1, 1, 
+        
+        ScrollView:  
+            MDBoxLayout:
+                orientation: "vertical"
+                spacing: "10dp"
+                size_hint_y: None
+                height: self.minimum_height
+    
+                MDLabel:
+                    text: "Edit Profile"
+                    halign: "center"
+                    valign: "center"
+                    bold: True
+                    size_hint_y: None
+                    height: "40dp"
+                    color: rgba(34, 34, 34, 255)
+    
+                MDCard:
+                    orientation: "vertical"
+                    padding: "8dp"
+                    size_hint: None, None
+                    size: "290dp", "100dp"
+                    pos_hint: {"center_x": .5, "center_y": .85}
+    
+                    Image:
+                        source: "assets/boy.png"
+                        size_hint: .5, .8
+                        pos_hint: {"center_x": .5, "center_y": .85}
+    
+                MDButton:
+                    pos_hint: {"center_x": .5, "center_y": .68}
+    
+                    MDButtonText:
+                        text: "Edit profile photo"
+    
+                MDBoxLayout:
+                    orientation: "vertical"
+                    spacing: "7dp"
+                    adaptive_height: True
+                    size_hint_x: .9
+                    pos_hint: {"center_x": .5, "center_y": .3}
+    
+                    MDLabel:
+                        text: "Name"
+                        bold: True
+                        size_hint_y: None
+                        height: "10dp"
+    
+                    MDTextField:
+                        mode: "outlined"
+    
+                    MDLabel:
+                        text: "Course"
+                        bold: True
+                        size_hint_y: None
+                        height: "10dp"
+    
+                    MDTextField:
+                        mode: "outlined"
+    
+                    MDLabel:
+                        text: "Year of Study"
+                        bold: True
+                        size_hint_y: None
+                        height: "10dp"
+    
+                    MDTextField:
+                        mode: "outlined"
+    
+                    MDLabel:
+                        text: "Registration Number"
+                        bold: True
+                        size_hint_y: None
+                        height: "10dp"
+    
+                    MDTextField:
+                        mode: "outlined"
+    
+                    MDLabel:
+                        text: "Student Number"
+                        bold: True
+                        size_hint_y: None
+                        height: "10dp"
+    
+                    MDTextField:
+                        mode: "outlined"
+    
+                MDButton:
+                    style: "filled"
+                    theme_width: "Custom"
+                    height: "40dp"
+                    size_hint: .66, .065
+                    pos_hint: {"center_x": .5, "center_y": .18}
+                    on_release:
+                        root.manager.transition.direction = "left"
+                        root.manager.current = ""
+    
+                    MDButtonText:
+                        id: text
+                        text: "SUBMIT"
+                        pos_hint: {"center_x": .5, "center_y": .5}
 '''
 
 class MainScreen(Screen):
@@ -505,6 +617,9 @@ class ResetScreen(Screen):
 class SignupScreen(Screen):
     pass
 
+class StudentSetupScreen(Screen):
+    pass
+
 class ClassCheckInApp(MDApp):
 
     def build(self):
@@ -514,6 +629,7 @@ class ClassCheckInApp(MDApp):
         screen_manager.add_widget(LoginScreen(name="login"))
         screen_manager.add_widget(ResetScreen(name="reset"))
         screen_manager.add_widget(SignupScreen(name="signup"))
+        screen_manager.add_widget(StudentSetupScreen(name="studentSetup"))
 
         return Builder.load_string(KV)
 
