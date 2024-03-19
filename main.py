a87cf25f2ea449d7fba708183fe8af37f7743197
Olaki Edwin <olakiedwin@gmail.com>
2024-03-19 16:48:25 +0300

    Lecturer Info screen added to main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ ScreenManager:
     LecturerResetScreen:
     LecturerSignupScreen:
     StudentInfoScreen:
+    LecturerInfoScreen:
 
 <MainScreen>
     name: "main"
@@ -640,6 +641,9 @@ ScreenManager:
             height: "40dp"
             size_hint: .66, .065
             pos_hint: {"center_x": .5, "center_y": .34}
+            on_release:
+                root.manager.transition.direction = "left"
+                root.manager.current = "lecturerInfo"
 
             MDButtonText:
                 id: text
@@ -881,7 +885,7 @@ ScreenManager:
             pos_hint: {"center_x": .5, "center_y": .3} 
             on_release:
                 root.manager.transition.direction = "left"
-                root.manager.current = " "
+                root.manager.current = "lecturerInfo"
 
             MDButtonText:
                 id: text
@@ -1038,6 +1042,120 @@ ScreenManager:
                 id: text
                 text: "SUBMIT"
                 pos_hint: {"center_x": .5, "center_y": .5}
+                
+<LecturerInfoScreen>:
+    name: "lecturerInfo"
+    
+    MDFloatLayout:
+        md_bg_color: 1, 1, 1, 1
+                    
+        MDLabel:
+            text: "W e l c o m e !"
+            bold: True
+            font_size: "26sp"
+            pos_hint: {"center_x": .8, "center_y": .95}
+            color: rgba(0, 0, 59, 255)    
+                
+        MDLabel:
+            text: "Enter your details below to continue!"
+            bold: True
+            font_size: "18sp"
+            pos_hint: {"center_x": .6, "center_y": .88}
+            color: rgba(255, 200, 130, 255)      
+                
+        MDFloatLayout:
+            size_hint: .7, .07
+            pos_hint: {"center_x": .5, "center_y": .78}
+                
+            TextInput:
+                hint_text: "Full Names"
+                size_hint_y: .75
+                pos_hint: {"center_x": .43, "center_y": .5}
+                background_color: 1, 1, 1, 0
+                foreground_color: rgba(0, 0, 59, 255)
+                cursor_color: rgba(0, 0, 59, 255)
+                font_size: "14sp"
+                cursor_width: "2sp"
+                multiline: False
+                    
+            MDFloatLayout:
+                pos_hint: {"center_x": .45, "center_y": 0}
+                size_hint_y: .03
+                md_bg_color: rgba(178, 178, 178, 255)
+                    
+        MDFloatLayout:
+            size_hint: .7, .07
+            pos_hint: {"center_x": .5, "center_y": .65}
+                
+            TextInput:
+                hint_text: "Faculty"
+                size_hint_y: .75
+                pos_hint: {"center_x": .43, "center_y": .5}
+                background_color: 1, 1, 1, 0
+                foreground_color: rgba(0, 0, 59, 255)
+                cursor_color: rgba(0, 0, 59, 255)
+                font_size: "14sp"
+                cursor_width: "2sp"
+                multiline: False
+                    
+            MDFloatLayout:
+                pos_hint: {"center_x": .45, "center_y": 0}
+                size_hint_y: .03
+                md_bg_color: rgba(178, 178, 178, 255)
+                    
+        MDFloatLayout:
+            size_hint: .7, .07
+            pos_hint: {"center_x": .5, "center_y": .52}
+                
+            TextInput:
+                hint_text: "Department"
+                size_hint_y: .75
+                pos_hint: {"center_x": .43, "center_y": .5}
+                background_color: 1, 1, 1, 0
+                foreground_color: rgba(0, 0, 59, 255)
+                cursor_color: rgba(0, 0, 59, 255)
+                font_size: "14sp"
+                cursor_width: "2sp"
+                multiline: False
+                    
+            MDFloatLayout:
+                pos_hint: {"center_x": .45, "center_y": 0}
+                size_hint_y: .03
+                md_bg_color: rgba(178, 178, 178, 255)
+                    
+        MDFloatLayout:
+            size_hint: .7, .07
+            pos_hint: {"center_x": .5, "center_y": .38}
+                
+            TextInput:
+                hint_text: "Lecturer Email"
+                size_hint_y: .75
+                pos_hint: {"center_x": .43, "center_y": .5}
+                background_color: 1, 1, 1, 0
+                foreground_color: rgba(0, 0, 59, 255)
+                cursor_color: rgba(0, 0, 59, 255)
+                font_size: "14sp"
+                cursor_width: "2sp"
+                multiline: False
+                    
+            MDFloatLayout:
+                pos_hint: {"center_x": .45, "center_y": 0}
+                size_hint_y: .03
+                md_bg_color: rgba(178, 178, 178, 255)  
+                    
+        MDButton:
+            theme_width: "Custom"
+            height: "40dp"
+            size_hint: .66, .065
+            pos_hint: {"center_x": .5, "center_y": .20}
+            on_release:
+                root.manager.transition.direction = "left"
+                root.manager.current = ""
+                
+            MDButtonText:
+                id: text
+                text: "SUBMIT"
+                pos_hint: {"center_x": .5, "center_y": .5}
 '''
 
 
@@ -1078,6 +1196,9 @@ class LecturerSignupScreen(Screen):
 class StudentInfoScreen(Screen):
     pass
 
+class LecturerInfoScreen(Screen):
+    pass
+
 
 class ClassCheckInApp(MDApp):
 
@@ -1093,6 +1214,7 @@ class ClassCheckInApp(MDApp):
         screen_manager.add_widget(StudentResetScreen(name="lecturerReset"))
         screen_manager.add_widget(StudentSignupScreen(name="lecturerSignup"))
         screen_manager.add_widget(StudentInfoScreen(name="studentInfo"))
+        screen_manager.add_widget(LecturerInfoScreen(name="LecturerInfo"))
 
         return Builder.load_string(KV)
 
